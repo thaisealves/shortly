abde5e8b4da6db838ad0da35634e7e17ff92689f
thaisealves <alves.thaisesilva@gmail.com>
2022-08-06 18:29:41 -0300

    making some configs

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+const PORT = process.env.PORT || 4000;
+app.listen (PORT, ()=> console.log(`Server running on PORT ${PORT}`))
