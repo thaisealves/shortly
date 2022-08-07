758794a78bd651a95c4035f6abf98850f951ca1b
thaisealves <alves.thaisesilva@gmail.com>
2022-08-06 23:45:19 -0300

    feat: sign up route created, with repository pattern

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/routesIndex.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.use(router);
 
 const PORT = process.env.PORT || 4000;
-app.listen (PORT, ()=> console.log(`Server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
