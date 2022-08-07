e12f6796a2e054afc996a1525fd09079148f4439
thaisealves <alves.thaisesilva@gmail.com>
2022-08-07 15:12:27 -0300

    feat: getting the url by the id on params

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -10,3 +10,13 @@ export async function postShorten(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function getShortenById(req, res) {
+  const { id } = req.params;
+  const result = await urlsRepository.getShortenById(id);
+  if (result.rowCount === 0) {
+    return res.status(404).send("ID de url inexistente!");
+  } else {
+    return res.status(200).send(result.rows[0]);
+  }
+}
